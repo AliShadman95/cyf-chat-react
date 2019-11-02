bce34fa66008cad6c47a8f4399bf2724555744b8
AliShadman95 <alishadman955@gmail.com>
2019-11-02 02:56:24 +0100

    changed cors

--- ChatAPI/index.js
+++ ChatAPI/index.js
@@ -3,7 +3,17 @@ dotenv.config();
 var cors = require("cors");
 const express = require("express");
 const app = express();
-app.use(cors());
+var whitelist = ["http://example1.com", "http://example2.com"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+app.use(cors(corsOptions));
 var socketio = require("socket.io");
 var http = require("http");
 const PORT = process.env.PORT || 3005;
