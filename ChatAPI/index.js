ef32433049ce1c88f5f29c8b05e7398121321a66
AliShadman95 <alishadman955@gmail.com>
2019-11-02 04:27:27 +0100

    fr

--- ChatAPI/index.js
+++ ChatAPI/index.js
@@ -14,19 +14,19 @@ var Task = require("./api/models/chatModel"); //created model loading here
 var bodyParser = require("body-parser");
 
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+   handlePreflightRequest: (req, res) => {
+     const headers = {
+       "Access-Control-Allow-Headers": "Content-Type",
+       "Access-Control-Allow-Origin": "https://letschaaat.netlify.com",
+       "Access-Control-Allow-Methods": "GET,UPDATE,OPTIONS,POST"
+     };
+     res.writeHead(200, headers);
+     res.end();
+   };
+ });
 
-//  {
-//    handlePreflightRequest: (req, res) => {
-//      const headers = {
-//        "Access-Control-Allow-Headers": "Content-Type",
-//        "Access-Control-Allow-Origin": req.headers.origin,
-//        "Access-Control-Allow-Methods": "GET,UPDATE,OPTIONS,POST"
-//      };
-//      res.writeHead(200, headers);
-//      res.end();
-//    };
-//  }
+
 
 const {
   addUser,
