bdd374d93c950d60b9ab0e19afd5626fbfbcbe42
AliShadman95 <alishadman955@gmail.com>
2019-11-02 01:35:15 +0100

    changed cors

--- ChatAPI/index.js
+++ ChatAPI/index.js
@@ -13,7 +13,17 @@ var cors = require("cors");
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": req.headers.origin,
+      "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
+  }
+});
 
 const {
   addUser,
