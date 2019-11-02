d186422243235278ef03ea1564b9e0a6137a2b0e
AliShadman95 <alishadman955@gmail.com>
2019-11-02 03:37:33 +0100

    t

--- ChatAPI/index.js
+++ ChatAPI/index.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
   const origin = req.get("origin");
   res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "OPTIONS,GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma"
