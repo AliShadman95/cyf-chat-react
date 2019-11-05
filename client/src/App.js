638ecb2ddb25ff362bdcf192b223767a491d7a86
alishadman95 <alishadman955@gmail.com>
2019-11-05 02:24:51 +0100

    fixs

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 class App extends React.Component {
