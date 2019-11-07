b20eb95084475d999b2bce747df4c3c657254914
AliShadman95 <alishadman955@gmail.com>
2019-11-07 19:16:32 +0100

    need to fix rooms messages

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,10 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 class App extends React.Component {
